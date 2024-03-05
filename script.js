f99ab9489ef0e164c0d63130027618671e18cdfb
Valou <116378179+A1oneeee@users.noreply.github.com>
2024-03-05 16:04:32 +0100

    <Update> Ajout d'une vérification de size des fichier avec une page d'erreur + ajout des fichier correct dans une liste

--- script.js
+++ script.js
@@ -2,17 +2,37 @@ const fileDrop = document.getElementById('msg');
 const deposit = document.getElementById('deposit');
 const depositP = document.getElementById('depositP');
 const cancel = document.getElementById('cancel');
+const sender = document.getElementById('sender');
+const cancelError = document.getElementById('errorCancel');
+const errorFile = document.getElementById('errorFile');
+
+let maListe = [];
 
 // Empêcher les événements dragenter et dragover par défaut pour permettre le dépôt
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     deposit.addEventListener(eventName, preventDefaults, false);
 });
 
-function preventDefaults(event) {
+/*
+ * Event for the cancel interface with the files deposit
+ */
+cancel.addEventListener('click', () => {
+    deposit.style.display = 'none';
+});
+cancelError.addEventListener('click', () => {
+    errorFile.style.display = 'none';
+});
+sender.addEventListener('click', () => {
     event.preventDefault();
-    event.stopPropagation();
-}
+    maListe.forEach(function(element){
+        console.log(element.name);
+    })
+});
 
+/* #====== OBSERVERS ======#
+ * EventListener for the dragFiles
+ * That will show and unshow the deposit interface
+ */
 fileDrop.addEventListener('dragenter', (event) => {
     event.preventDefault();
     deposit.style.display = 'flex';
@@ -26,15 +46,21 @@ deposit.addEventListener('drop', (event) => {
     handleFiles(files);
 });
 
+/*
+ * Functions use here
+ */
 function handleFiles(files) {
     // Traitement des fichiers ici
     for (const file of files) {
-        console.log('Nom du fichier:', file.name);
-        console.log('Type du fichier:', file.type);
-        console.log('Taille du fichier:', file.size, 'octets');
+        if(file.size > 5000){
+            errorFile.style.display = 'flex';
+            continue;
+        }
+        maListe.push(file);
     }
 }
+function preventDefaults(event) {
+    event.preventDefault();
+    event.stopPropagation();
+}
 
-cancel.addEventListener('click', () => {
-    deposit.style.display = 'none';
-});
